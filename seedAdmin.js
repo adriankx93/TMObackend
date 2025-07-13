224a08f4ae4f82b31fade7fddefbcaf865237e2e
adriankx93 <kicioradrian@gmail.com>
2025-07-13 19:34:14 +0200

    Update seedAdmin.js

--- seedAdmin.js
+++ seedAdmin.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const User = require('./models/User');
-
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/tmo');
-
 async function createAdmin() {
-  const exists = await User.findOne({ email: 'admin' });
-  if (!exists) {
-    await User.create({
-      firstName: 'Super',
-      lastName: 'Admin',
-      email: 'admin',
-      password: 'adrian128',
-      role: 'Admin'
-    });
-    console.log('Admin utworzony!');
-  } else {
-    console.log('Admin już istnieje.');
+  try {
+    const exists = await User.findOne({ email: 'admin' });
+    if (!exists) {
+      await User.create({
+        firstName: 'Super',
+        lastName: 'Admin',
+        email: 'admin',
+        password: 'adrian128',
+        role: 'Admin'
+      });
+      console.log('Admin utworzony!');
+    } else {
+      console.log('Admin już istnieje.');
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    mongoose.disconnect();
+    process.exit();
   }
-  process.exit();
 }
 createAdmin();
