68b07892b117f27712c2f5b0501d9ff2082599be
adriankx93 <kicioradrian@gmail.com>
2025-07-14 12:06:37 +0200

    Update warehouse.js

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const WarehouseItem = require("../models/WarehouseItem");
-const auth = require("../middleware/auth");
+const { auth } = require("../middleware/auth");
 
 router.get("/items", auth, async (req, res) => {
     try {
