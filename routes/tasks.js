4cfc142155481a95edea37967af01b1a7920b307
adriankx93 <kicioradrian@gmail.com>
2025-07-14 12:09:44 +0200

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task");
-const auth = require("../middleware/auth");
+const { auth } = require("../middleware/auth");
 
 // Get all tasks
 router.get("/", auth, async (req, res) => {
